5932c4cd1d851b688c035ec57228e5596282be13
hangEX7749 <hanghang7749@gmail.com>
2024-04-03 18:49:07 +0800

    ninja_id project init

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,85 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(title: 'Flutter Demo', home: Home()));
+void main() => runApp(const MaterialApp(
+      title: 'Ninja ID card',
+      home: NinjaCard(),
+    ));
 
-class Home extends StatelessWidget {
-  const Home({super.key});
-
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Ninja'),
-        centerTitle: true,
-        backgroundColor: Colors.blue[500],
-      ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              children: [
-                Expanded(
-                  flex: 8,
-                  child: Image.asset('assets/frieren.jpg')
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: const Text('Ninja ID Card'),
+          centerTitle: true,
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+        ),
+        body: const Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/frieren.jpg'),
+                  radius: 40.0,
                 ),
-                Expanded(
-                  flex: 3, //Percentage width of the row container
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
-                    color: Colors.grey,
-                    child: const Text("1"),
-                  ),
+              ),
+              Divider(
+                height: 90.0,
+                color: Color.fromARGB(255, 66, 66, 66),
+              ),
+              Text(
+                'NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
                 ),
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
-                    color: Colors.green,
-                    child: const Text("2"),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'MAMARDUKER',
+                style: TextStyle(
                     color: Colors.amber,
-                    child: const Text("3"),
-                  ),
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 30.0),
+              Text(
+                'CURRENT NINJA LEVEL',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
                 ),
-              ],
-            ),
-          ]),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        foregroundColor: Colors.grey[788],
-        backgroundColor: Colors.yellow[400],
-        child: const Text('click'),
-      ),
-    );
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                '7',
+                style: TextStyle(
+                    color: Colors.amber,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 30.0),
+              Row(children: <Widget>[
+                Icon(Icons.email, color: Color.fromARGB(255, 210, 208, 208)),
+                SizedBox(width: 10.0),
+                Text(
+                  'hang7749@mail.com',
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 210, 208, 208),
+                      fontSize: 18.0,
+                      letterSpacing: 1.0),
+                )
+              ])
+            ],
+          ),
+        ));
   }
 }
