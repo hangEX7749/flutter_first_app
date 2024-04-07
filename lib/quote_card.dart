6c4ef063a22aae4e827c068b904d8327eb2d5a50
hangEX7749 <hanghang7749@gmail.com>
2024-04-07 18:36:52 +0800

    function, parameter

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -4,10 +4,12 @@ class QuoteCard extends StatelessWidget {
   const QuoteCard({
     super.key,
     required this.quote,
+    required this.delete
   });
 
   // ignore: prefer_typing_uninitialized_variables
   final quote;
+  final Function() delete;
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +25,11 @@ class QuoteCard extends StatelessWidget {
             SizedBox(height: 6.0),
             Text(quote.author,
                 style: TextStyle(fontSize: 14, color: Colors.grey[800])),
-            SizedBox(height: 6.0),
+            SizedBox(height: 8.0),
+            TextButton.icon(
+                onPressed: delete,
+                icon: Icon(Icons.delete),
+                label: Text('Delete Quote'))
           ],
         ),
       ),
