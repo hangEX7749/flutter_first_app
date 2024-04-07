c138d1b4d0c4c8df9a8f527d7f2448e6a7ea504d
hangEX7749 <hanghang7749@gmail.com>
2024-04-07 17:51:23 +0800

    custom class

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
       home: QuoteList(),
     ));
 
 class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _QuoteListState createState() => _QuoteListState();
 }
 
 class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-    'Be yourself',
-    'I want it that way',
-    'Keep calm',
+  List<Quote> quotes = [
+    // 'Be yourself',
+    // 'I want it that way',
+    // 'Keep calm',
+    Quote(text: 'Oh No', author: 'hang'),
+    Quote(text: 'Oh No2', author: 'hang2'),
+    Quote(text: 'Oh No3', author: 'hang3'),
   ];
 
   @override
@@ -26,7 +33,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
       ),
     );
   }
---
+++ lib/quote.dart
@@ -0,0 +1,8 @@
+class Quote {
+  String text;
+  String author;
+
+  Quote({required this.text, required this.author});
+}
+
+Quote muquote = Quote(text: 'This is the quote text', author: 'oscar wilde');
