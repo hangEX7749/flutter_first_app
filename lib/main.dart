195ac49c378585f9d7c9d3a28f78639eec7c611a
hangEX7749 <hanghang7749@gmail.com>
2024-04-07 17:35:05 +0800

    list

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-      title: 'Ninja ID card',
-      home: NinjaCard(),
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
     ));
 
-class NinjaCard extends StatefulWidget {
-   const NinjaCard({super.key});
-
+class QuoteList extends StatefulWidget {
   @override
-  State<NinjaCard> createState() => _NinjaCardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _NinjaCardState extends State<NinjaCard> {
-  double ninjaLevel = 0;
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'Be yourself',
+    'I want it that way',
+    'Keep calm',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[900],
-        appBar: AppBar(
-          title: const Text('Ninja ID Card'),
-          centerTitle: true,
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.grey[850],
-          elevation: 0.0,
-        ),
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/frieren.jpg'),
-                  radius: 40.0,
-                ),
-              ),
-              Divider(
-                height: 90.0,
-                color: Color.fromARGB(255, 66, 66, 66),
-              ),
-              Text(
-                'NAME',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              SizedBox(height: 10.0),
-              Text(
-                'MAMARDUKER',
-                style: TextStyle(
-                    color: Colors.amber,
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 30.0),
-              Text(
-                'CURRENT NINJA LEVEL',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              SizedBox(height: 10.0),
-              Text(
-                "$ninjaLevel",
-                style: TextStyle(
-                    color: Colors.amber,
-                    letterSpacing: 2.0,
-                    fontSize: 28.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 30.0),
-              Row(children: const <Widget>[
-                Icon(Icons.email, color: Color.fromARGB(255, 210, 208, 208)),
-                SizedBox(width: 10.0),
-                Text(
-                  'hang7749@mail.com',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 210, 208, 208),
-                      fontSize: 18.0,
-                      letterSpacing: 1.0),
-                )
-              ])
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                ninjaLevel += 1;
-              });
-            },
-            backgroundColor: Colors.amber[800],
-            child: const Icon(Icons.add)));
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
+      ),
+    );
   }
 }
